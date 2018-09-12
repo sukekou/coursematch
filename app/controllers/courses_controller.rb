455852288a4d1487ddff314e32b4f22c81673ceb
sukekou <sukekou1031@gmail.com>
2018-09-12 22:54:38 +0900

    11

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,5 +3,6 @@ class CoursesController < ApplicationController
   def show
     @course=Course.find(params[:id])
     @users=@course.users
+    @old_users = @users.select{|user| user.updated_at.day != Date.today.day}
   end
 end
